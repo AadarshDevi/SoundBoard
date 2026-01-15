dad82b6c2921d06816ff2b676d331c6fef06bdc1
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-01-15 12:04:29 -0800

    removed a file

--- rewrite.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-if b'AlpharinoGamer947@gmail.com' in commit.author_email or b'AlpharinoGamer947@gmail.com' in commit.committer_email:
-    commit.author_name = b'Aadarsh Devi'
-    commit.author_email = b'aadarsh123456char@gmail.com'
-    commit.committer_name = b'Aadarsh Devi'
-    commit.committer_email = b'aadarsh123456char@gmail.com'
